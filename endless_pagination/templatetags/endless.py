a944c505668b26ced934978c79faaa96fca4dc34
Guillaume Gérard <guillaume.gerard@novapost.fr>
2014-11-24 22:25:59 +0100

    refs #54 -- Loading on scroll to table

--- endless_pagination/templatetags/endless.py
+++ endless_pagination/templatetags/endless.py
@@ -366,6 +366,31 @@ def show_more(context, label=None, loading=settings.LOADING):
     return {}
 
 
+@register.inclusion_tag('endless/show_more_table.html', takes_context=True)
+def show_more_table(context, label=None, loading=settings.LOADING):
+
+    """Show the link to get the next page in a Twitter-like pagination in a
+    template for table.
+
+    Usage::
+
+        {% show_more_table %}
+
+    Alternatively you can override the label passed to the default template::
+
+        {% show_more_table "even more" %}
+
+    You can override the loading text too::
+
+        {% show_more_table "even more" "working" %}
+
+    Must be called after ``{% paginate objects %}``.
+    """
+    # This template tag could raise a PaginationError: you have to call
+    # *paginate* or *lazy_paginate* before including the showmore template.
+    return show_more(context, label, loading)
+
+
 @register.tag
 def get_pages(parser, token):
     """Add to context the list of page links.
